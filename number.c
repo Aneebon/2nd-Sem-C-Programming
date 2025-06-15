a1d4e1f88f0c5631a61e8b2539a7e87f89323d3c
Aneebon <156197136+Aneebon@users.noreply.github.com>
2025-06-15 22:51:29 +0530

    Delete number.c

--- number.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main(){
-    int myNumbers[10]={1,2,3,4,5,6,7,8,9,10};
-    for (int i = 0; i < 10; i++)
-    {
-        printf("The number at index %d is %d\n", i, myNumbers[i]);
-    }
-    
-}
